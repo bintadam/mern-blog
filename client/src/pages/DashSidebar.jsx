df170b0ee065625ecbc1f18eadabaf1a28aecff3
bintadam <zakiyaadan12@gmail.com>
2024-06-05 21:21:45 +0200

    complete profile page UI

--- client/src/pages/DashSidebar.jsx
+++ client/src/pages/DashSidebar.jsx
@@ -18,7 +18,7 @@ export default function DashSidebar() {
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Link to='/dashboard?tab=profile'>
+                    <Link  to='/dashboard?tab=profile'>
                         <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor='dark'>
                             Profile
                         </Sidebar.Item>
